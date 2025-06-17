7e5d9ee44cbc06011bc3ae78b45e4b54075ead9a
robbiew6002 <robbiewhitty@gmail.com>
2025-06-17 13:42:04 +0100

    Create basic update asset feature.

---
+++ scripts/update.py
@@ -0,0 +1,17 @@
+#File to hold update record scripts
+
+from supabaseClient import supabase
+
+def update_asset(asset_id,asset_details):
+    asset_details=dict(asset_details)
+    current_details=supabase.table("devices").select('*').eq('id', asset_id).execute().data[0]
+    if asset_details["hostname"]:
+        response=supabase.table("devices").update({"hostname":asset_details["hostname"]}).eq('id', asset_id).execute()
+    if int(asset_details["type_id"]) != current_details["type_id"]:
+        response=supabase.table("devices").update({"type_id":int(asset_details["type_id"])}).eq('id', asset_id).execute()
+    if int(asset_details["customer_id"]) != current_details["customer_id"]:
+        response=supabase.table("devices").update({"customer_id":int(asset_details["customer_id"])}).eq('id', asset_id).execute()
+    if int(asset_details["status_id"]) != current_details["status_id"]:
+        response=supabase.table("devices").update({"status_id":int(asset_details["status_id"])}).eq('id', asset_id).execute()
+    print(response)
+    return response.data[0]
